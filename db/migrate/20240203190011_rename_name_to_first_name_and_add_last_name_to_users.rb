e71e7cc9d7d9a981db4868ec565f529c6fecbb0d
nishan-fs <nishan.boyaju@flagship.cc>
2024-02-04 16:42:17 +0545

    [ADD] added users migration file

---
+++ db/migrate/20240203190011_rename_name_to_first_name_and_add_last_name_to_users.rb
@@ -0,0 +1,8 @@
+class RenameNameToFirstNameAndAddLastNameToUsers < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :users, :name
+
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+  end
+end
