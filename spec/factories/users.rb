188bc8eb5ff79b7208114adaab8fe00d8645adf0
nishan-fs <nishan.boyaju@flagship.cc>
2024-02-05 01:35:43 +0545

    [ADD] updated configuration and added factory_bots for user and content

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    country { Faker::Address.country }
+    password { "password" }
+  end
+end
